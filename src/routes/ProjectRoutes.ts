492f90976b0ec393e67da95320bc293d467317f2
ErnestoCapambo <ernestosikilitacapambo@gmail.com>
2024-05-22 23:43:44 +0100

    Adicionando a rota para pegar arquivo do projeto

--- src/routes/ProjectRoutes.ts
+++ src/routes/ProjectRoutes.ts
@@ -2,12 +2,15 @@ import upload from "../middlewares/FileUpload";
 import { createProject } from "../project/createProject";
 import { deleteProject } from "../project/deleteProject";
 import { getProject } from "../project/getProject";
+import { getProjectFile } from "../project/getProjectFile";
 import { updateProject } from "../project/updateProject";
 import router from "./UserRoutes";
 
 router.post("/create-project/:userId?", upload.single('file'), createProject)
 
 router.get("/get-project/:projectId?", getProject)
+
+router.get('/get-project-file/:projectId?', getProjectFile)
 
 router.put("/update-project/:projectId?", upload.single('file'), updateProject)
 
